411308adc43fa995f9a259615bc6e3d53824c2a4
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 14:45:56 +0200

    Add files via upload

---
+++ 12.JavaBooleanDataTypes/src/main/java/com/mycompany/javabooleandatatypes/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabooleandatatypes;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);  //Outputs true
+        System.out.println(isFishTasty); //outputs false
+    }
+}
